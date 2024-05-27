f579c31e60d37151c97891e7ec2a7322d914c853
Humble Scholar <69286782+xiaonan-37021@users.noreply.github.com>
2024-05-27 11:28:26 +0800

    Update daily_sign.py

--- daily_sign.py
+++ daily_sign.py
@@ -1,7 +1,7 @@
 import os
 import requests
 
-cookie = os.environ.get("JD_COOKIE")
+cookie = os.environ.get("JD_COOKIE", "").strip()
 print(cookie)
 
 url = ("https://api.m.jd.com/client.action?functionId=signBeanAct&body=%7B%22fp%22%3A%22-1%22%2C%22shshshfp%22%3A%22-1"
